499c199e509b51503a6bde895224ed61325b5bfb
EkaterinaI-tech <hikatepeace@gmail.com>
2026-04-22 22:17:29 +0300

    RegistrationTest, LoginTest is ready and auth, cart Test before PO

--- src/test/java/ru/ivanbulgakov/demowebshop/test/CartTest.java
+++ src/test/java/ru/ivanbulgakov/demowebshop/test/CartTest.java
@@ -1,19 +1,17 @@
 package ru.ivanbulgakov.demowebshop.test;
 
 import com.codeborne.selenide.Configuration;
-import net.datafaker.Faker;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import ru.ivanbulgakov.demowebshop.pages.WsRegistrationPage;
 import ru.ivanbulgakov.demowebshop.steps.AuthSteps;
 
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static ru.ivanbulgakov.demowebshop.config.Config.WEBSHOP_URL;
-import static ru.ivanbulgakov.demowebshop.config.Config.WEBSHOP_URL_REGISTER_URL;
 
 public class CartTest {
-    private static final Faker faker = new Faker();
     private final AuthSteps authSteps = new AuthSteps();
 
     @BeforeEach
@@ -31,5 +29,23 @@ public class CartTest {
         $$("ul.top-menu li a").get(1).hover();
         $(byText("Desktops")).click();
         $$("div.product-grid div").get(0).click();
+
+        String itemName = $("[itemprop=name]").getText();
+        String itemPrice = $("[itemprop=price]").getText();
+        String itemQuantity = "2";
+
+        $$("dl dd ul li").get(0).$$("input").get(0).click();
+        $("input.qty-input").setValue(itemQuantity);
+        $("input.add-to-cart-button").click();
+        $(".bar-notification.success").shouldBe(visible);
+        $("span.cart-qty").shouldHave(text("(" + itemQuantity + ")"));
+        $("span.cart-label").click();
+
+        $("a.product-name").shouldHave(text(itemName));
+        String actualQuantity  = $(".qty-input").val();
+        assertEquals(itemQuantity, actualQuantity);
+        $("span.product-subtotal").shouldHave(text(String.valueOf(
+                Float.parseFloat(itemPrice) * Float.parseFloat(itemQuantity))));
+
     }
 }
